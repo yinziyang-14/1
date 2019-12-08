d9e4e7093e0d9d9f20f3e69d1b158ddc8e411b4e
yinziyang-14 <57909787+yinziyang-14@users.noreply.github.com>
2019-12-09 01:06:35 +0800

    设计的学生类（父类）

---
+++ student.java
@@ -0,0 +1,17 @@
+package xitong；
+public class student 
+{
+	String    Id;
+	String   Name;
+	String   Sex;
+	String getId() {
+		    return Id;
+		  }
+	String getName() {
+		    return Name;
+		  }
+	String getSex() {
+		    return Sex;
+		  }
+	  
+}                                          //定义父类
